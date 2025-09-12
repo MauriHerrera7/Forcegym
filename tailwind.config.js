537c2b1710d953abed343dc8a8813880222e5ac0
MauriHerrera7 <mauriherrera457@gmail.com>
2025-09-12 16:29:07 -0300

    Primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.{html,svg}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./hooks/**/*.{js,ts,jsx,tsx}",
+    "./providers/**/*.{js,ts,jsx,tsx}",
+    "./utils/**/*.{js,ts,jsx,tsx}",
+    "./auth/**/*.{js,ts,jsx,tsx}",
+    "./contact/**/*.{js,ts,jsx,tsx}",
+    "./dashboard/**/*.{js,ts,jsx,tsx}",
+    "./home/**/*.{js,ts,jsx,tsx}",
+    "./profile/**/*.{js,ts,jsx,tsx}",
+    "./subscripcion/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
